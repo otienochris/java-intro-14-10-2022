a6604ea191e92acd2f5644f0f2ccd4165682ff7b
otienochris <otienochris98@gmail.com>
2022-10-14 04:48:06 +0300

    data types

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @author christopherochiengotieno@gmail.com
+ * @version 1.0.0
+ * @since Friday, 14/10/2022
+ */
+public class Animal {
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
