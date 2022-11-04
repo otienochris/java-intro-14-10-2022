b1b66f265d00e9ff2017b696e8c6df31cc290fcf
otienochris <otienochris98@gmail.com>
2022-11-04 18:45:32 +0300

    adding test starter code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,94 @@
 package org.example;
 
+import org.example.model.Student;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int choice = 1;
+
+        Scanner in = new Scanner(System.in);
+
+        do {
+            System.out.println("\n===============================================\n");
+            System.out.println("Unit Testing Java and Spring boot applications\n");
+            System.out.println("===============================================\n");
+
+            getMenu();
+            choice = in.nextInt();
+
+            switch (choice){
+                case 1:
+                    registerStudent(in);
+                    break;
+                case 2:
+                    System.out.println("Details");
+                    break;
+                case 3:
+                    System.out.println("Finance");
+                    break;
+                case 4:
+                    System.out.println("Accommodation");
+                    break;
+                case 5:
+                    System.out.println("Units");
+                    break;
+                default:
+                    choice = 1000;
+
+            }
+
+            System.out.println(choice);
+            System.out.flush();
+
+        } while (choice != 0);
+        in.close();
+    }
+
+    private static Student registerStudent(Scanner in) {
+        System.out.println("Enter Student Details Below: \n");
+        System.out.println("***************************\n");
+
+        // get reg no
+        System.out.print("Registration Number >> ");
+        String registrationNumber = in.nextLine();
+        System.out.println();
+
+        // get first name
+        System.out.print("First Name >> ");
+        String firstName = in.nextLine();
+        System.out.println();
+
+        // get second name
+        System.out.print("Second Name >> ");
+        String secondName = in.nextLine();
+        System.out.println();
+
+        // get academic year
+        System.out.print("Academic Year >> ");
+        Integer year = in.nextInt();
+        System.out.println();
+
+        // get semester
+        System.out.print("Semester >> ");
+        Integer sem = in.nextInt();
+        System.out.println();
+
+        Student student = new Student(registrationNumber, firstName, secondName,year, sem);
+        return student;
+    }
+
+    private static void getMenu() {
+
+        System.out.println("1. Register");
+        System.out.println("2. View user details");
+        System.out.println("3. Register Finance");
+        System.out.println("4. Register Accommodation");
+        System.out.println("5. Register Units");
+        System.out.println("0. Exit Application");
+        System.out.println("\n===============================================\n");
+        System.out.print(">>\t");
     }
 }
---
+++ src/main/java/org/example/model/Student.java
@@ -0,0 +1,119 @@
+package org.example.model;
+
+import java.util.Objects;
+
+/**
+ * @author christopherochiengotieno@gmail.com
+ * @version 1.0.0
+ * @since Friday, 04/11/2022
+ */
+public class Student {
+
+    private String registrationNumber;
+    private String firstName;
+    private String lastName;
+    private Integer academicYear;
+    private Integer semester;
+    private Boolean hasDoneFinanceRegistration = false;
+    private Boolean hasDoneAccommodationRegistration = false;
+    private Boolean hasDoneUnitRegistration = false;
+
+    public Student(String registrationNumber, String firstName, String lastName, Integer academicYear, Integer semester) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.academicYear = academicYear;
+        this.semester = semester;
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAcademicYear() {
+        return academicYear;
+    }
+
+    public void setAcademicYear(Integer academicYear) {
+        this.academicYear = academicYear;
+    }
+
+    public Integer getSemester() {
+        return semester;
+    }
+
+    public void setSemester(Integer semester) {
+        this.semester = semester;
+    }
+
+    public Boolean getHasDoneFinanceRegistration() {
+        return hasDoneFinanceRegistration;
+    }
+
+    public void setHasDoneFinanceRegistration(Boolean hasDoneFinanceRegistration) {
+        this.hasDoneFinanceRegistration = hasDoneFinanceRegistration;
+    }
+
+    public Boolean getHasDoneAccommodationRegistration() {
+        return hasDoneAccommodationRegistration;
+    }
+
+    public void setHasDoneAccommodationRegistration(Boolean hasDoneAccommodationRegistration) {
+        this.hasDoneAccommodationRegistration = hasDoneAccommodationRegistration;
+    }
+
+    public Boolean getHasDoneUnitRegistration() {
+        return hasDoneUnitRegistration;
+    }
+
+    public void setHasDoneUnitRegistration(Boolean hasDoneUnitRegistration) {
+        this.hasDoneUnitRegistration = hasDoneUnitRegistration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return Objects.equals(getRegistrationNumber(), student.getRegistrationNumber()) || (Objects.equals(getFirstName(), student.getFirstName()) && Objects.equals(getLastName(), student.getLastName()) && Objects.equals(getAcademicYear(), student.getAcademicYear()) && Objects.equals(getSemester(), student.getSemester()) && Objects.equals(getHasDoneFinanceRegistration(), student.getHasDoneFinanceRegistration()) && Objects.equals(getHasDoneAccommodationRegistration(), student.getHasDoneAccommodationRegistration()) && Objects.equals(getHasDoneUnitRegistration(), student.getHasDoneUnitRegistration()));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getAcademicYear(), getSemester(), getHasDoneFinanceRegistration(), getHasDoneAccommodationRegistration(), getHasDoneUnitRegistration());
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", academicYear=" + academicYear +
+                ", semester=" + semester +
+                ", hasDoneFinanceRegistration=" + hasDoneFinanceRegistration +
+                ", hasDoneAccommodationRegistration=" + hasDoneAccommodationRegistration +
+                ", hasDoneUnitRegistration=" + hasDoneUnitRegistration +
+                '}';
+    }
+}
